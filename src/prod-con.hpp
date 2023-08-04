91b4db5d180a048d6f728fa6ac3c3b19be7504d0
Tom Peterka <tpeterka@mcs.anl.gov>
2023-08-04 12:46:31 -0500

    WIP adding moab mesh to producer

--- src/prod-con.hpp
+++ src/prod-con.hpp
@@ -5,6 +5,11 @@
 #include    <thread>
 #include    <mutex>
 
+// diy
+#include    <diy/mpi/communicator.hpp>
+#include    <diy/decomposition.hpp>
+#include    <diy/assigner.hpp>
+
 // lowfive
 #include    <lowfive/vol-metadata.hpp>
 #include    <lowfive/vol-dist-metadata.hpp>
@@ -12,19 +17,35 @@
 #include    <lowfive/H5VOLProperty.hpp>
 
 // moab
-#include "iMesh.h"
-#include "MBiMesh.hpp"
-#include "moab/Core.hpp"
-#include "moab/Range.hpp"
-#include "MBTagConventions.hpp"
-#include "moab/ParallelComm.hpp"
-#include "moab/HomXform.hpp"
-#include "moab/ReadUtilIface.hpp"
+#include    "iMesh.h"
+#include    "MBiMesh.hpp"
+#include    "moab/Core.hpp"
+#include    "moab/Range.hpp"
+#include    "MBTagConventions.hpp"
+#include    "moab/ParallelComm.hpp"
+#include    "moab/HomXform.hpp"
+#include    "moab/ReadUtilIface.hpp"
 
 using communicator  = MPI_Comm;
 using diy_comm      = diy::mpi::communicator;
+using Bounds        = diy::DiscreteBounds;
 
 namespace l5        = LowFive;
 
 enum {producer_task, producer1_task, producer2_task, consumer_task, consumer1_task, consumer2_task};
+
+#define ERR {if(rval!=MB_SUCCESS)printf("MOAB error at line %d in %s\n", __LINE__, __FILE__);}
+
+using namespace moab;
+
+void hex_mesh_gen(int *mesh_size, Interface *mbint, EntityHandle *mesh_set,
+        ParallelComm *mbpc, diy::RegularDecomposer<Bounds>& decomp, diy::Assigner& assign);
+void tet_mesh_gen(int *mesh_size, Interface *mbint, EntityHandle *mesh_set,
+        ParallelComm *mbpc, diy::RegularDecomposer<Bounds>& decomp, diy::Assigner& assign);
+void create_hexes_and_verts(int *mesh_size, Interface *mbint, EntityHandle *mesh_set,
+        diy::RegularDecomposer<Bounds>& decomp, diy::Assigner& assign, ParallelComm* mbpc);
+void create_tets_and_verts(int *mesh_size, Interface *mbint, EntityHandle *mesh_set,
+        diy::RegularDecomposer<Bounds>& decomp, diy::Assigner& assign, ParallelComm* mbpc);
+void resolve_and_exchange(Interface *mbint, EntityHandle *mesh_set, ParallelComm *mbpc);
+
 
